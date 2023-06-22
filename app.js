d75431bb49ca77f3b58135ac8536e910f9321fa6
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-06-22 09:14:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -72,7 +72,6 @@ app.delete("/users/:id", async (request, response) => {
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
-
     console.log(`App listening on http://localhost:${port}`);
     console.log(`Users Endpoint http://localhost:${port}/users`);
 });
