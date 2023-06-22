cd7b233a2a8db10361d03402e9bfcc7b002537af
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-06-22 12:53:19 +0200

    with dovenv

--- database.js
+++ database.js
@@ -1,13 +1,14 @@
 import mysql from "mysql2"; // using mysql2 - an install npm library
+import "dotenv/config";
 
 // using the variables from the .env file
 // and creates the connection to database
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    database: "users",
-    password: "database",
+    host: process.env.HOST,
+    port: process.env.DATABASE_PORT,
+    user: process.env.MYSQL_USER,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
     multipleStatements: true
 });
 
