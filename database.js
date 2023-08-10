f039992777ec2b1417e1fd100a8a140ab77ec606
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-08-10 11:49:01 +0200

    ssl settings added

--- database.js
+++ database.js
@@ -1,5 +1,6 @@
 import mysql from "mysql2"; // using mysql2 - installed npm library
 import "dotenv/config";
+import fs from "fs";
 
 // using the variables from the .env file
 // and creates the connection to database
@@ -9,7 +10,7 @@ const connection = mysql.createConnection({
     user: process.env.MYSQL_USER,
     database: process.env.MYSQL_DATABASE,
     password: process.env.MYSQL_PASSWORD,
-    multipleStatements: true
+    ssl: { cs: fs.readFileSync("DigiCertGlobalRootCA.crt.pem") }
 });
 
 // exports database connection
