39c759e88a18b4c2771d602ab3b526764ed0464f
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-11-13 10:22:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import cors from "cors";
 import express from "express";
-// import db from "./database.js";
+import db from "./database.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +9,7 @@ app.use(express.json()); // To parse JSON bodies
 app.use(cors()); // Enable CORS for all routes
 
 app.get("/", (request, response) => {
+    db.ping();
     response.send("Node.js Users REST API 🎉");
 });
 
