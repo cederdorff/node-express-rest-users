5c0c30fc4ec938a32e93ff75ce928c0ad70a92b9
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-01-12 21:50:01 +0100

    Refactor database connection and update package.json start script

--- database.js
+++ database.js
@@ -1,15 +1,14 @@
 import mysql from "mysql2/promise"; // using mysql2 - installed npm library
-import "dotenv/config";
 
 // using the variables from the .env file
 // and creates the connection to database
 const db = await mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    database: process.env.MYSQL_DATABASE,
-    password: process.env.MYSQL_PASSWORD,
-    multipleStatements: true
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  database: process.env.MYSQL_DATABASE,
+  password: process.env.MYSQL_PASSWORD,
+  multipleStatements: true
 });
 
 // exports database connection
